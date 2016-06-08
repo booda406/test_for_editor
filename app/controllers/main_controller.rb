d0abb73c7c66dad84541a383bfb27795d3eabff7
Steven Tseng <booda406@gmail.com>
2016-06-08 18:23:39 +0800

    backend render json & change image url

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -9,7 +9,7 @@ class MainController < ApplicationController
 		filename = image.original_filename
 		File.open(File.join(Rails.root, 'public', 'images', filename), 'wb') { |f| f.write image.tempfile.read }
 		url = Rails.root + '/public' + '/images' + filename
-		json = "{'uploaded': 1, 'fileName': #{filename}, 'url': #{url}}"
+		json = {uploaded: 1, fileName: filename, url: url}.to_json
 		render json: json
 	end
 
